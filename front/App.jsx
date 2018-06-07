2bd394034000280718b993990e06c47e7531a4aa
Deplais Vincent <vincent.deplais@orange.fr>
2018-06-07 10:52:06 +0200

    Init front folder

---
+++ front/App.jsx
@@ -0,0 +1,15 @@
+import './styles/_reset.scss';
+import './styles/_general.scss';
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div>Bonjour</div>;
+  }
+}
+
+export default App;
