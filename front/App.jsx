41d8eaafd7df3a61cac46636bd28e7999615c1ad
Benjamin NGOUM <benjamin.ngoum@gmail.com>
2018-06-07 17:06:02 +0200

    wip

--- front/App.jsx
+++ front/App.jsx
@@ -3,8 +3,6 @@ import './styles/_general.scss';
 import React from 'react';
 import Home from './components/Home/Home';
 
-import PageProfil from './components/PageProfil/PageProfil';
-
 class App extends React.Component {
   constructor() {
     super();
--- front/components/PageProfil/PageProfil.jsx
+++ front/components/PageProfil/PageProfil.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 import axios from 'axios';
 
-import { Button, Icon, Badge, Avatar, CircularProgress } from '@material-ui/core/';
+import { Button, Icon, Badge, Avatar, CircularProgress, Dialog } from '@material-ui/core/';
 
 class PageProfil extends React.Component {
   constructor() {
@@ -12,44 +12,84 @@ class PageProfil extends React.Component {
 
   render() {
     return (
-      <main className="wrapper-page-profil">
-        <div className="wrapper-header-profil">
-            <div className="wrapper-avatar-profil">
-                <Avatar className="avatarProfil">JD</Avatar>
+        <main className="wrapper-page-profil">
+            <div className="wrapper-header-profil">
+                <div className="wrapper-avatar-profil">
+                    <Avatar className="avatarProfil">JD</Avatar>
+                </div>
+
+                <div className="wrapper-info-user">
+                    <h2 className="nomProfil">Jean DUPONT</h2>
+                    <p className="metierProfil">Graphic Designer</p>
+                    <p className="entrepriseProfil">Freelance</p>
+                    <p className="mailProfil">jean.dupont@gmail.com</p>
+                    <p className="numberProfil">06.12.05.27.94</p>
+                    <p className="siteWebProfil"><a href="#">jean-dupont.fr</a></p>
+                </div>
+
+                <div className="wrapper-competences-user">
+                    <div className="wrapper-badge-competences-user">
+                        <span>3</span>
+                        <CircularProgress variant="static" value={90} className="badge-competences-user" />
+                    </div>
+                    <div className="wrapper-badge-competences-user">
+                        <span>3</span>
+                        <CircularProgress variant="static" value={50} className="badge-competences-user" />
+                    </div>
+                    <div className="wrapper-badge-competences-user">
+                        <span>3</span>
+                        <CircularProgress variant="static" value={60} className="badge-competences-user" />
+                    </div>
+
+                    <div className="wrapper-bio-user">
+                        <p>Bio: </p>
+                        <p className="text-bio-user">Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem</p>
+                    </div>
+                </div>
+
+                <button className="btn-close"></button>
             </div>
 
-            <div className="wrapper-info-user">
-                <h2 className="nomProfil">Jean DUPONT</h2>
-                <p className="metierProfil">Graphic Designer</p>
-                <p className="entrepriseProfil">Freelance</p>
-                <p className="mailProfil">jean.dupont@gmail.com</p>
-                <p className="numberProfil">06.12.05.27.94</p>
-                <p className="siteWebProfil"><a href="#">jean-dupont.fr</a></p>
+            <div className="wrapper-body-profil">
+                <div className="wrapper-competences-profil">
+                    <h3>Compétences</h3>
+
+                    <div className="wrapper-badge-competence">
+                        <p className="badge-competence">#UX</p>
+                    </div>
+
+                    <div className="wrapper-badge-competence">
+                        <p className="badge-competence">#DA</p>
+                    </div>
+
+                    <div className="wrapper-badge-competence">
+                        <p className="badge-competence">#TEXTILE</p>
+                    </div>
+
+                    <div className="wrapper-badge-competence">
+                        <p className="badge-competence">#PRINT</p>
+                    </div>
+                </div>
+
+                <div className="wrapper-aides-profil">
+                    <h3>Jean peut aider sur :</h3>
+
+                    <div className="wrapper-badge-aide">
+                        <p className="badge-aide">UX</p>
+                    </div>
+
+                    <div className="wrapper-badge-aide">
+                        <p className="badge-aide">La découpe laser</p>
+                    </div>
+
+                    <div className="wrapper-badge-aide">
+                        <p className="badge-aide">Illustrator</p>
+                    </div>
+                </div>
             </div>
 
-            <div className="wrapper-competences-user">
-                <div className="wrapper-badge-competences-user">
-                    <span>3</span>
-                    <CircularProgress variant="static" value={90} className="badge-competences-user" />
-                </div>
-                <div className="wrapper-badge-competences-user">
-                    <span>3</span>
-                    <CircularProgress variant="static" value={50} className="badge-competences-user" />
-                </div>
-                <div className="wrapper-badge-competences-user">
-                    <span>3</span>
-                    <CircularProgress variant="static" value={60} className="badge-competences-user" />
-                </div>
-
-                <div>
-                    <p>Bio: </p>
-                    <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem</p>
-                </div>
-            </div>
-
-            <button className="btn-close"></button>
-        </div>
-      </main>
+            <Button className="btn-contacter" variant="contained">Contacter</Button>
+        </main>
     );
   }
 }
