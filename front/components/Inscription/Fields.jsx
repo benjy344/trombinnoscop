95cd0c337949b36b7bc8b27ae1a0c9d7bcd7e2d7
Elmarino <elmar.atavares@gmail.com>
2018-06-07 15:13:08 +0200

    Profile Sign in

---
+++ front/components/Inscription/Fields.jsx
@@ -0,0 +1,21 @@
+const Fields = ()=>{
+    <div>
+        <label>
+        Nom:
+        <input type="text" name="name" />
+    </label>
+    <div>
+        <label>
+            <span>Résident</span>
+            <input type="radio" name="myRadioInput" value="Option A"/>
+        </label>
+
+        <label>
+            <span></span>
+            <input type="radio" name="myRadioInput" value="Option B"/>
+        </label>
+    </div>
+    </div>
+    
+    
+}
